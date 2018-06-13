7b1101f0089b409bbf859f84c0100a34a6fbc799
Jack Stinchcombe <stinchjack@gmail.com>
2018-06-14 11:09:02 +1200

    fixes

--- controller.php
+++ controller.php
@@ -3,14 +3,17 @@
 namespace Concrete\Package\Helper; //<--must match package name
 use Page;
 use Helper\ComposerJson\PackageInfo;
+
+
+
 class Controller extends \Package
 {
 
-
-    //protected $pkgHandle = 'helper'; //<--must match package name
+    protected $pkgHandle = 'exporter'; //<--must match package name
     protected $appVersionRequired = '8.3.2';
+    protected $pkgVersion = '0.3.33';
 
-    //protected $pkgVersion = '0.3.21';
+
     // see https://documentation.concrete5.org/developers/packages/adding-custom-code-to-packages
     protected $pkgAutoloaderRegistries = array(
         'src/Helper/Permissions/' => '\Helper\Permissions',
@@ -18,25 +21,14 @@ class Controller extends \Package
         'src/Helper/Block/' => '\Helper\Block'
     );
 
-    private function getComposerInfo() {
-      if (!isset($this->packageInfo)) {
-        $this->packageInfo = new PackageInfo($this->getPackagePath());
-      }
-      return $this->packageInfo;
-    }
-
-    public function getPackageVersion() {
-      return $this->getComposerInfo()->getVersion();
-    }
-
     public function getPackageDescription()
     {
-        return t($this->getComposerInfo()->getDescription());
+        return ('bananas');
     }
 
     public function getPackageHandle()
     {
-        return t($this->getComposerInfo()->getHandle());
+        return 'helper';
     }
 
     public function getPackageName()
