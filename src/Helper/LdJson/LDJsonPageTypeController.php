736a0278954c27b3185479bfd278bd0e296e0183
Jack Stinchcombe <stinchjack@gmail.com>
2018-06-18 21:02:00 +1200

    added class LDJsonPageTypeController

---
+++ src/Helper/LdJson/LDJsonPageTypeController.php
@@ -0,0 +1,20 @@
+<?php
+namespace Helper\LdJson;
+
+use Helper\LdJson\LdJsonTrait;
+use Concrete\Core\Page\Controller\PageTypeController;
+
+abstract class LDJsonPageTypeController extends PageTypeController {
+
+  use LdJsonTrait;
+
+  public function on_start() {
+
+    $this->makeSchema();
+    parent::on_start();
+
+
+  }
+
+}
+?>
